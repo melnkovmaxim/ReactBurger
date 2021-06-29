3d2747f64436adb89d1cb4c2519e95d25269de24
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-06-29 19:28:31 +1000

    merge sprint-1/step-1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,21 +4,37 @@ import data from "../../utils/data";
 import AppHeader from "../AppHeader/AppHeader";
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
+import OrderDetails from "../OrderDetails/OrderDetails";
 
 function App() {
-  const ingredients = data;
+  const [ingredients, setIngredients] = React.useState([]);
+  const [error, setError] = React.useState(null);
 
-  return (
-    <div className={componentStyles.content}>
-      <div className={componentStyles.header}>
-        <AppHeader />
-      </div>
-      <div className={componentStyles.body}>
-        <BurgerIngredients ingredients={ingredients} />
-        <div className="ml-5 mr-5" />
-        <BurgerConstructor ingredients={ingredients} />
-      </div>
-    </div>
+  React.useEffect(() => {
+    fetch('https://norma.nomoreparties.space/api/ingredients')
+          .then(response => response.json())
+          .then(json => setIngredients(json.data))
+          .catch(error => setError(error));
+  }, []);
+
+  return (  
+    <OrderDetails />  
+    // <>
+    //   {ingredients
+    //     ? (
+    //       <div className={componentStyles.content}>
+    //         <div className={componentStyles.header}>
+    //           <AppHeader />
+    //         </div>
+    //         <div className={componentStyles.body}>
+    //           <BurgerIngredients ingredients={ingredients} />
+    //           <div className="ml-5 mr-5" />
+    //           <BurgerConstructor ingredients={ingredients} />
+    //         </div>
+    //       </div>
+    //     )
+    //     : (<div>{error}</div>)
+    //   }
   );
 }
 
