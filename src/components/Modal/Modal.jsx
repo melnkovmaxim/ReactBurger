442f5dc5917549cf868aa3eaab8a6104856ed54e
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-08-07 14:36:41 +1000

    поправил ModalOverlay

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,28 @@
 import componentStyles from "./Modal.module.css";
-import { useEffect, useCallback } from "react";
+import { useCallback } from "react";
 import ModalOverlay from "../ModalOverlay/ModalOverlay";
 import ReactDOM from "react-dom";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 const Modal = ({ children }) => {
   const history = useHistory();
-  const keyCodeEsc = 27;
 
-  const closeOnClick = useCallback((e) => {
-    if (e.target.className === componentStyles.overlay) {
-      history.goBack();
-    }
-  }, [history]);
-
-  const closeOnPress = useCallback((e) => {
-    if (e.keyCode === keyCodeEsc) {
-      history.goBack();
-    }
-  }, [history]);
-
-  useEffect(() => {
-    document.addEventListener("keydown", closeOnPress);
-
-    return () => {
-      document.removeEventListener("keydown", closeOnPress);
-    };
-  }, [closeOnPress]);
+  const closeOnClick = useCallback(
+    (e) => {
+      if (e.target.className === componentStyles.overlay) {
+        history.goBack();
+      }
+    },
+    [history]
+  );
 
   return ReactDOM.createPortal(
-    <div className={componentStyles.overlay} onClick={closeOnClick}>
-      <ModalOverlay>
+    <>
+      <ModalOverlay history={history} />
+      <div className={componentStyles.overlay} onClick={closeOnClick}>
         {children}
-      </ModalOverlay>
-    </div>,
+      </div>
+    </>,
     document.getElementById("modal")
   );
 };
--- src/components/ModalOverlay/ModalOverlay.jsx
+++ src/components/ModalOverlay/ModalOverlay.jsx
@@ -1,33 +1,27 @@
+import { useEffect } from "react";
+import { useCallback } from "react";
 import componentStyles from "./ModalOverlay.module.css";
-import PropTypes from "prop-types";
-import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import { Link } from 'react-router-dom';
 
-const ModalOverlay = ({ children, header }) => {
-  return (
-    <div className={componentStyles.container}>
-    {header ? (
-      <div className={componentStyles.headerWrapper}>
-        <div className={`mt-10 ml-10 mr-10 ${componentStyles.header}`}>
-          <p className="text text_type_main-large">{header}</p>
-          <div className={`mr-10 ${componentStyles.closeButton}`}>
-            <Link to='/'><CloseIcon type="primary" /></Link>
-          </div>
-        </div>
-      </div>
-    ) : (
-      <div className={`mt-15 mr-10 ${componentStyles.closeButton}`}>
-        <CloseIcon type="primary" />
-      </div>
-    )}
-    <div>{children}</div>
-  </div>
+const ModalOverlay = ({ history }) => {
+  const keyCodeEsc = 27;
+  const closeOnPress = useCallback(
+    (e) => {
+      if (e.keyCode === keyCodeEsc) {
+        history.goBack();
+      }
+    },
+    [history]
   );
-};
 
-ModalOverlay.propTypes = {
-  onClose: PropTypes.func,
-  header: PropTypes.string,
+  useEffect(() => {
+    document.addEventListener("keydown", closeOnPress);
+
+    return () => {
+      document.removeEventListener("keydown", closeOnPress);
+    };
+  }, [closeOnPress]);
+
+  return (<div className={componentStyles.overlay}></div>);
 };
 
 export default ModalOverlay;
