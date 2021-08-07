12c812590a754b987db6d336ac721f4c152c64d3
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-08-07 12:19:45 +1000

    добавил ссылку перехода на главную страницу по клику на логотип

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -4,7 +4,7 @@ import {
   Logo,
   ProfileIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import HeaderButton from "../HeaderButton/HeaderButton";
 import componentStyles from "./AppHeader.module.css";
 
@@ -15,28 +15,29 @@ const AppHeader = () => {
       <header className={`pt-4 pb-4 ${componentStyles.header}`}>
         <nav className={componentStyles.stretchedFlexItem}>
           <NavLink exact to="/" activeClassName={componentStyles.active}>
-            <HeaderButton
-              type="secondary"
-              size="medium"
-              text="Конструктор"
-            >
+            <HeaderButton type="secondary" size="medium" text="Конструктор">
               <BurgerIcon />
             </HeaderButton>
           </NavLink>
-          <NavLink to="/profile/orders" activeClassName={componentStyles.active}>
-            <HeaderButton
-              type="secondary"
-              size="medium"
-              text="Лента заказов"
-            >
+          <NavLink
+            to="/profile/orders"
+            activeClassName={componentStyles.active}
+          >
+            <HeaderButton type="secondary" size="medium" text="Лента заказов">
               <ListIcon type="secondary" />
             </HeaderButton>
           </NavLink>
         </nav>
-        <Logo />
+        <Link to="/">
+          <Logo />
+        </Link>
         <div className={componentStyles.stretchedFlexItem}>
           <div className={componentStyles.buttonWrapper}>
-            <NavLink exact to="/profile" activeClassName={componentStyles.active}>
+            <NavLink
+              exact
+              to="/profile"
+              activeClassName={componentStyles.active}
+            >
               <HeaderButton
                 type="secondary"
                 size="medium"
