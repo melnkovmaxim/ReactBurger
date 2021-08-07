0ad8b5a9696468865d1ab392ec0180ab688769df
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-08-07 17:51:25 +1000

    добавил кнопки в профиле 'Отмена' и 'Сохранить' с обработчиками

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -1,15 +1,16 @@
 import componentStyles from "./ProfileDetails.module.css";
-import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Button, Input, PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getUserInfo } from "../../services/actions/ProfileActions";
+import { getUserInfo, updateUserInfo } from "../../services/actions/ProfileActions";
 import { getAccessToken } from "../../utils/Cookie";
 
 const ProfileDetails = () => {
   const dispatch = useDispatch();
   const user = useSelector(store => store.profile.user);
   const [state, setState] = useState({ name: '', email: '', password: '' } );
+  const [previousState, setPreviousState] = useState();
   const onChange = (e) => setState({ ...state, [e.target.name]: e.target.value });
   const accessToken = getAccessToken();
 
@@ -19,11 +20,27 @@ const ProfileDetails = () => {
 
   useEffect(() => {
     setState({ ...state, ...user });
+    setPreviousState({ ...state, ...user });
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (e.nativeEvent.submitter.innerText.toLowerCase() === 'отмена') {
+      return;
+    }
+
+    dispatch(updateUserInfo(state));
+    setPreviousState(state);
+  };
+
+  const onCancel = () => {
+    setState(previousState);
+  };
+
   return (
-    <form className={`ml-15 pl-6 pr-6 ${componentStyles.container}`}>
+    <form className={`ml-15 pl-6 pr-6 ${componentStyles.container}`} onSubmit={onSubmit}>
       <div className={`${componentStyles.inputWrapper}`}>
         <Input
           type={"text"}
@@ -47,7 +64,7 @@ const ProfileDetails = () => {
         />
       </div>
       <div className={`mt-6 ${componentStyles.inputWrapper}`}>
-        <Input
+        <PasswordInput
           type={"text"}
           placeholder={"Пароль"}
           onChange={onChange}
@@ -56,6 +73,12 @@ const ProfileDetails = () => {
           name={"password"}
           value={state.password}
         />
+      </div>
+      <div className={`mt-6 ${componentStyles.buttons}`}>
+        <div className={componentStyles.disableSubmit}>
+          <Button type="secondary" name="cancel" onClick={onCancel}>Отмена</Button>
+        </div>
+        <Button type="primary" name="submit">Сохранить</Button>
       </div>
     </form>
   );
