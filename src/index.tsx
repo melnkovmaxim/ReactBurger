459028004773729f0714c27dcd39714f9b8b27ed
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-08-29 17:49:52 +1000

    типизация middleware

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {compose, createStore, applyMiddleware} from 'redux';
-import {rootReducer} from './services/reducers/RootReducer';
+import { Provider } from 'react-redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import SocketMiddleware from './middlewares/SocketMiddleware';
-import {allOrdersWsActions, userOrdersWsActions} from './services/actions/WsActions';
+import { allOrdersWsActions, userOrdersWsActions } from './services/actions/WsActions';
+import createSocketMiddleware from "./middlewares/SocketMiddleware";
+import { rootReducer } from "./services/reducers/RootReducer";
 
 declare global {
   interface Window {
@@ -19,18 +19,22 @@ declare global {
 const wsAllOrdersUrl: string = 'wss://norma.nomoreparties.space/orders/all';
 const wsUserOrdersUrl: string = 'wss://norma.nomoreparties.space/orders';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
+
+const publicOrdersMiddleware = createSocketMiddleware(wsAllOrdersUrl, allOrdersWsActions);
+const privateOrdersMiddleware = createSocketMiddleware(wsUserOrdersUrl, userOrdersWsActions, true);
+
 const enhancer = composeEnhancers(
-  applyMiddleware(thunk),
-  applyMiddleware(SocketMiddleware(wsAllOrdersUrl, { ...allOrdersWsActions })),
-  applyMiddleware(SocketMiddleware(wsUserOrdersUrl, { ...userOrdersWsActions }, true))
+  applyMiddleware(thunk, publicOrdersMiddleware, privateOrdersMiddleware)
 );
 
 const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={ store }>
       <App/>
     </Provider>
   </React.StrictMode>,
--- src/middlewares/SocketMiddleware.ts
+++ src/middlewares/SocketMiddleware.ts
@@ -1,45 +1,49 @@
 import { getAccessToken } from "../utils/Cookie";
 import { IOrdersWebSocketActions } from "../interfaces/middlewares/IOrdersWebSocketActions";
+import { RootState } from "../services/reducers/RootReducer";
+import { Middleware } from "redux";
 
-const socketMiddleware = (wsUrl: string, wsActions: IOrdersWebSocketActions, withToken: boolean = false) => {
-  return store => {
-    let socket: WebSocket | null = null;
+const createSocketMiddleware = (wsUrl: string, wsActions: IOrdersWebSocketActions, withToken: boolean = false): Middleware<{}, RootState> => {
+  const socketMiddleware: Middleware<{}, RootState> = (store) => {
+      let socket: WebSocket | null = null;
 
-    return next => action => {
-      const { dispatch } = store;
-      const { type } = action;
-      const { wsInit, onOpen, onClose, onError, onMessage } = wsActions;
+      return next => action => {
+        const { dispatch } = store;
+        const { type } = action;
+        const { wsInit, onOpen, onClose, onError, onMessage } = wsActions;
 
-      if (type === wsInit) {
-        const url: string = withToken ? `${wsUrl}?token=${getAccessToken()}` : wsUrl;
-        socket = new WebSocket(url);
-      }
+        if (type === wsInit) {
+          const url: string = withToken ? `${wsUrl}?token=${getAccessToken()}` : wsUrl;
+          socket = new WebSocket(url);
+        }
 
-      if (socket) {
-        socket.onopen = event => {
-          dispatch({ type: onOpen, payload: event });
-        };
+        if (socket) {
+          socket.onopen = event => {
+            dispatch({ type: onOpen, payload: event });
+          };
 
-        socket.onerror = event => {
-          dispatch({ type: onError, payload: event });
-        };
+          socket.onerror = event => {
+            dispatch({ type: onError, payload: event });
+          };
 
-        socket.onmessage = event => {
-          const { data } = event;
-          const parsedData = JSON.parse(data);
-          const { success, ...restParsedData } = parsedData;
+          socket.onmessage = event => {
+            const { data } = event;
+            const parsedData = JSON.parse(data);
+            const { success, ...restParsedData } = parsedData;
 
-          dispatch({ type: onMessage, payload: restParsedData });
-        };
+            dispatch({ type: onMessage, payload: restParsedData });
+          };
 
-        socket.onclose = event => {
-          dispatch({ type: onClose, payload: event });
-        };
-      }
+          socket.onclose = event => {
+            dispatch({ type: onClose, payload: event });
+          };
+        }
 
-      next(action);
+        next(action);
+      };
     };
-  };
+
+  return socketMiddleware;
 };
 
-export default socketMiddleware;
+export default createSocketMiddleware;
